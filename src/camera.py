52fc01d974825d73cc4cb83f59dc744980b2758e
Lam Wen Zhi Clarence <clarence_lam_wz@aiap.sg>
2022-01-03 04:26:27 +0800

    added flask app for record/screenshot

---
+++ src/camera.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+import peekingduck
+from threading import Thread
+
+
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+
+try:
+    os.mkdir('./uploads')
+except OSError as error:
+    pass
+
+app = Flask(__name__, template_folder='./templates')
+camera = cv2.VideoCapture(0)
+
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame
+    while True:
+        success, frame = camera.read() 
+        if success:
+            if(face):                
+                frame= detect_face(frame)
+            if(grey):
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if(neg):
+                frame=cv2.bitwise_not(frame)    
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                p = os.path.sep.join(['uploads', "IMG_{}.png".format(str(now).replace(":",''))])
+                cv2.imwrite(p, frame)
+            
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+    
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+                
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port=8000)
+    # For production mode, comment the line above and uncomment below. Then run at the terminal: waitress-serve --port=8000 app:app
+    # serve(app, host="0.0.0.0", port=8000)   # using waitress
+    # app.run(host="0.0.0.0")
+    
+camera.release()
+cv2.destroyAllWindows()     
